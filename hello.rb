e28d69f0e5801d412a06c4e20cd7f80055d7efdc
catherine <vuthi.esther@gmail.com>
2022-07-13 14:26:28 +0300

    into to ruby

---
+++ hello.rb
@@ -0,0 +1,32 @@
+#Single Line Comment
+# My first Ruby App - single line comment
+
+#multi line comment
+
+=begin
+    this is a
+    multi-line Ruby
+    comment
+=end
+
+# Displaying Ruby Output - puts and print strings
+ puts "Hello World!" # most prefered syntax
+ puts ("Hello World!")
+ puts "My name is Cate"
+
+ print "Hello World!"
+ print "My name is Cate"
+
+ puts ""
+
+ #Running Ruby application in the terminal
+ # ruby.filename.rb
+
+ # Outputting complex data using - p and pp
+ # Array
+ p [10, 20, 30, 40]
+
+ # Nested hashes
+
+ pp [{name: "Cate", age: 22}, {name: "Kyle", age: 30}, {name: "Joy", age: 25}]
+ 
